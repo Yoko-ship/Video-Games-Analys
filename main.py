e9218cac540182c7a66733af5420bd8fae55415a
User <yuko.brandon13@gmail.com>
2025-10-23 18:17:37 +0500

    added linear regression

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -33,7 +34,7 @@ class Games:
         plt.grid(True,linestyle='-',alpha=0.6)
         plt.show()
 
-    def k_Nearest_Neighbor(self):
+    def k_Nearest_Neighbor(self):        
         initial_x = self.cleanned[["Global_Sales","NA_Sales","EU_Sales","JP_Sales","Other_Sales"]].astype(np.int64)
         X = initial_x[self.cleanned['Global_Sales'] > 3]
         y = self.cleanned.loc[self.cleanned["Global_Sales"] > 3,"Genre"]
@@ -43,6 +44,22 @@ class Games:
         knn2.fit(X.values,y)
         res = knn2.predict(np.array([[40,29,3,6,0]]).reshape(1,-1))
 
+
+    def linear_regression(self):
+        df_encoded = pd.get_dummies(self.cleanned.loc[self.cleanned["NA_Sales"] > 3,["Platform",'Genre',"Publisher"]],drop_first=True)
+        
+        initial_x = self.cleanned[["NA_Sales","EU_Sales","JP_Sales","Other_Sales"]].astype(np.int64)
+        X = pd.concat([initial_x[self.cleanned['NA_Sales'] > 3],df_encoded],axis=1)
+        y = self.cleanned.loc[self.cleanned['NA_Sales'] > 3,"Global_Sales"]
+
+        X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=1)
+        model = LinearRegression()
+        model.fit(X_train,y_train)
+        prediction = model.predict(X_test)
+
+        print("Коэффициент влияние регионов на общую продажу")
+        print(f"Регион NA: {model.coef_[0]} \n Регион EU: {model.coef_[1]} \n Регион Япония: {model.coef_[2]} \n Другие: {model.coef_[3]}")
+        
 
     def handle_data(self):
         released_games_count = self.cleanned.groupby("Year")["Name"].count().reset_index()
@@ -75,6 +92,8 @@ class Games:
         self.draw_bar_chart(genre_games,"Genre","Name","Жанр","Количество","Количество выпущенных игр по жанру с 1980 - 2020")
 
         self.k_Nearest_Neighbor()
+        self.linear_regression()
+        
 
 if __name__ == "__main__":
     df = Games()
